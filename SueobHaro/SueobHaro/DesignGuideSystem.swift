448c3f5297d3a2d700604fb1e7c59e429b8d999f
eraser <eraser3031@gmail.com>
2022-07-18 23:36:55 +0900

    Feat: Color Guide System 추가

---
+++ SueobHaro/SueobHaro/DesignGuideSystem.swift
@@ -0,0 +1,28 @@
+//
+//  DesignGuideline.swift
+//  SueobHaro
+//
+//  Created by 김예훈 on 2022/07/18.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let spBlack = UIColor(named: "SPBlack")!
+    let spDarkBlue = UIColor(named: "SPDarkBlue")!
+    let spLightBlue = UIColor(named: "SPLightBlue")!
+    let spTurkeyBlue = UIColor(named: "SPTurkeyBlue")!
+    
+    let greyscale1 = UIColor(named: "Greyscale1")!
+    let greyscale2 = UIColor(named: "Greyscale2")!
+    let greyscale3 = UIColor(named: "Greyscale3")!
+    let greyscale4 = UIColor(named: "Greyscale4")!
+    let greyscale5 = UIColor(named: "Greyscale5")!
+    let greyscale6 = UIColor(named: "Greyscale6")!
+    let greyscale7 = UIColor(named: "Greyscale7")!
+}
