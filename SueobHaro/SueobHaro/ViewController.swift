5732314613279d414a1c319f3cec3d3e6bdba359
eraser <eraser3031@gmail.com>
2022-07-18 15:10:58 +0900

    Feat: collectionView layout 코드 추가

--- SueobHaro/SueobHaro/ViewController.swift
+++ SueobHaro/SueobHaro/ViewController.swift
@@ -9,13 +9,53 @@ import UIKit
 import SwiftUI
 
 class ViewController: UIViewController {
+    
+//    var dataSource: UICollectionViewDiffableDataSource<Section, Item>! = nil
 
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .clear
+        collectionView.delegate = self
+        collectionView.layer.cornerRadius = 20
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+        ])
     }
 
+    private func createLayout() -> UICollectionViewLayout {
+        
+        let sectionProvider = { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let section: NSCollectionLayoutSection
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(200))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(200))
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+            
+            section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 6, leading: 0, bottom: 6, trailing: 0)
+            
+            return section
+        }
+        let layout = UICollectionViewCompositionalLayout(sectionProvider: sectionProvider)
+        return layout
+    }
+}
 
+extension ViewController: UICollectionViewDelegate {
+    
 }
 
 struct PreView: PreviewProvider {
