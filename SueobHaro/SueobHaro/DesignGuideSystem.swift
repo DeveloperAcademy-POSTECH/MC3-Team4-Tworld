9608750da167e1fd72ee6175bf5852607bf0d4e1
Bethev <jmail88@naver.com>
2022-07-20 01:03:52 +0900

    Feat: SwiftUI Color extension 추가

--- SueobHaro/SueobHaro/DesignGuideSystem.swift
+++ SueobHaro/SueobHaro/DesignGuideSystem.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import SwiftUI
 
 // MARK: Color
 // self.view.backgroundColor = .theme.spLightBlue
@@ -27,6 +28,22 @@ struct ColorTheme {
     let greyscale5 = UIColor(named: "Greyscale5")!
     let greyscale6 = UIColor(named: "Greyscale6")!
     let greyscale7 = UIColor(named: "Greyscale7")!
+}
+
+// MARK: Color for swiftUI
+extension Color {
+    static let spBlack = Color("SPBlack")
+    static let spDarkBlue = Color("SPDarkBlue")
+    static let spLightBlue = Color("SPLightBlue")
+    static let spTurkeyBlue = Color("SPTurkeyBlue")
+    
+    static let greyscale1 = Color("Greyscale1")
+    static let greyscale2 = Color("Greyscale2")
+    static let greyscale3 = Color("Greyscale3")
+    static let greyscale4 = Color("Greyscale4")
+    static let greyscale5 = Color("Greyscale5")
+    static let greyscale6 = Color("Greyscale6")
+    static let greyscale7 = Color("Greyscale7")
 }
 
 // MARK: Font
