234e8e7c5d5c562ff0c78c62f8143d92cdd45284
Bethev <jmail88@naver.com>
2022-07-20 03:06:50 +0900

    Add: 신규수업 내용 확인 뷰 추가

---
+++ SueobHaro/SueobHaro/Views/ClassCheckView.swift
@@ -0,0 +1,20 @@
+//
+//  ClassCheckView.swift
+//  SueobHaro
+//
+//  Created by leejunmo on 2022/07/20.
+//
+
+import SwiftUI
+
+struct ClassCheckView: View {
+    var body: some View {
+        Text("전체 내용 확인")
+    }
+}
+
+struct ClassCheckView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClassCheckView()
+    }
+}
